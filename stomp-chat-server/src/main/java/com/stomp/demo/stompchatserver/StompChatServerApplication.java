0153bcc1b6cb2d0cdbb7767d6a914784a90120a2
qwe5507 <apvmffkdls@gmail.com>
2023-05-23 23:50:57 +0900

    stomp chat server init

---
+++ stomp-chat-server/src/main/java/com/stomp/demo/stompchatserver/StompChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.stomp.demo.stompchatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StompChatServerApplication.class, args);
+    }
+
+}
---
+++ stomp-chat-server/src/test/java/com/stomp/demo/stompchatserver/StompChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stomp.demo.stompchatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StompChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
