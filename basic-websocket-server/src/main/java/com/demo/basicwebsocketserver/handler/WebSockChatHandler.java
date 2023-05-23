df7f42236e4d55fb3c46bdf1942d7c0d32aaffe1
qwe5507 <apvmffkdls@gmail.com>
2023-05-23 23:05:17 +0900

    기본적인 websocket chat server 구현

--- basic-websocket-server/src/main/java/com/demo/basicwebsocketserver/handler/WebSockChatHandler.java
+++ basic-websocket-server/src/main/java/com/demo/basicwebsocketserver/handler/WebSockChatHandler.java
@@ -1,6 +1,11 @@
 package com.demo.basicwebsocketserver.handler;
 
 
+import com.demo.basicwebsocketserver.dto.ChatMessage;
+import com.demo.basicwebsocketserver.dto.ChatRoom;
+import com.demo.basicwebsocketserver.service.ChatService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -8,14 +13,18 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class WebSockChatHandler extends TextWebSocketHandler {
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
         log.info("payload {}", payload);
-        TextMessage textMessage = new TextMessage("Welcome chatting sever~^^");
-        session.sendMessage(textMessage);
+        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
+        room.handleActions(session, chatMessage, chatService);
     }
 }
