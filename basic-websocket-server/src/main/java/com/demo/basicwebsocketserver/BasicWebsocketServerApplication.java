a073a2a0fbdd78e79916ddc36675a3c13840ac43
qwe5507 <apvmffkdls@gmail.com>
2023-05-23 22:43:30 +0900

    init

---
+++ basic-websocket-server/src/main/java/com/demo/basicwebsocketserver/BasicWebsocketServerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.basicwebsocketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicWebsocketServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicWebsocketServerApplication.class, args);
+    }
+
+}
---
+++ basic-websocket-server/src/test/java/com/demo/basicwebsocketserver/BasicWebsocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.basicwebsocketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicWebsocketServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
