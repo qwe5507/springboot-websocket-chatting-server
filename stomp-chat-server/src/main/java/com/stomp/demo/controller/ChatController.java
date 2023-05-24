f2d96bc8aceb41b68b8fff3932f7c77061f3fc71
qwe5507 <apvmffkdls@gmail.com>
2023-05-24 10:33:04 +0900

    1차구현

---
+++ stomp-chat-server/src/main/java/com/stomp/demo/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.stomp.demo.controller;
+
+import com.stomp.demo.dto.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class ChatController {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.JOIN.equals(message.getType())) {
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        }
+
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+    }
+}
