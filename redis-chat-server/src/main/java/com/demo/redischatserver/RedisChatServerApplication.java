71eeb7bfe87beb8c451b77394b2dadeabb521a9f
qwe5507 <apvmffkdls@gmail.com>
2023-05-25 10:01:39 +0900

    init

---
+++ redis-chat-server/src/main/java/com/demo/redischatserver/RedisChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.redischatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisChatServerApplication.class, args);
+    }
+
+}
---
+++ redis-chat-server/src/test/java/com/demo/redischatserver/RedisChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.redischatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
